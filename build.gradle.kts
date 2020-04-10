f10948894a8c9074bb29892033b33b7dc991bff0
gscaparrotti <gscaparrotti@gmail.com>
2020-04-10 16:33:25 +0200

    remove unused import

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,11 +14,10 @@ repositories {
 }
 
 dependencies {
+	implementation(files("$rootDir/res"))
 	implementation("org.danilopianini:javalib-java7:[0, 1]")
 	implementation("com.miglayout:miglayout:[3, 4]")
-	implementation("jgoodies:forms:[1, 2]")
 	implementation("com.github.gscaparrotti:BenderModel:8efa72d")
-	implementation(files("./res"))
 	testImplementation("junit:junit:4.12")
 	testImplementation("com.vmlens:concurrent-junit:1.0.2")
 }
